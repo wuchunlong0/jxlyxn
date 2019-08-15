c7e1775810fc917b5fabe47dfb75960904817fef
wuchunlong0 <wcl6005@163.com>
2019-08-16 06:39:04 +0800

    demo

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,37 +1,35 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include 
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import RedirectView
-from django.contrib.auth import views as auth_views 
-#from django.urls import path
-from blog import views
 from django.contrib.auth import views as auth_views
-
+from django.views.generic import RedirectView
+from blog import views
+from django.views.generic import TemplateView, ListView, View
+class IndexView(TemplateView):
+    template_name = 'home_1/home.html'
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'login/', views.login, name="login"),
-    url(r'register/', views.register, name="register"),
-    url(r'logout/$', auth_views.LogoutView.as_view(), name='logout'), 
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/', views.login, name="login"),
+    url(r'^register/', views.register, name="register"),
+    url(r'^logout/', auth_views.LogoutView.as_view(), name='logout'), 
        
-    url(r'web/', include('web.urls')),
-    url(r'blog/', include('blog.urls')),#add
+    url(r'^web/', include('web.urls')),
+    url(r'^blog/', include('blog.urls')),#add
     url(r'^', RedirectView.as_view(url='/blog/index/')),
-
-    
-    
+        
 ]
